3565ff7ef930239466d9b8e6d94df6ea883da964
J-boardman <jaydeboardman@outlook.com>
2026-02-11 21:09:31 +1000

    02: database setup

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+const { data: tasks } = useConvexQuery(convexApi.tasks.get);
+
 </script>
 
 <template>
   <div>
     <h1>Hello World</h1>
     <UColorModeButton />
+    <pre>{{ tasks }}</pre>
   </div>
 </template>
